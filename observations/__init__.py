b3af2aa32c3058d61aa5628fbf2483d50dc10893
root <root@dthomas-centos.lco.gtn>
2014-12-03 13:38:30 -0800

    don't need to patch URLNode.render

--- observations/__init__.py
+++ observations/__init__.py
@@ -1,18 +1,9 @@
 #
-# monkey patch URLNode.render and urlresolvers.reverse,
-# so that they are aware of the application PREFIX
+# monkey patch urlresolvers.reverse,
+# to be aware of the application PREFIX
 #
-from django.template.defaulttags import URLNode
-import django.core.urlresolvers
 
-URLNode.old_render = URLNode.render
-def myrender(self, context):
-    from django.conf import settings
-    try:
-        return settings.PREFIX + self.old_render(context)
-    except:
-        return self.old_render(context)
-URLNode.render = myrender
+import django.core.urlresolvers
 
 old_reverse = django.core.urlresolvers.reverse
 def myreverse(viewname, urlconf=None, args=None, kwargs=None, prefix=None, current_app=None):
